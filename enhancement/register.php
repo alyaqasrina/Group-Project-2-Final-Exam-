1b7bd7b8ddfc6b5a9d732310cae482a8ec0c723a
Maryam2207 <154775061+Maryam2207@users.noreply.github.com>
2024-06-24 17:50:39 +0800

    Update register.php

--- enhancement/register.php
+++ enhancement/register.php
@@ -66,7 +66,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $mail->send();
 
-        // Insert user data into database
+        // Insert user data into database (SQL injection prevention)
         $sql = "INSERT INTO users (username, email, password, role, otp, is_verified) VALUES (?, ?, ?, ?, ?, false)";
         $stmt = $connection->prepare($sql);
         $stmt->bind_param('sssss', $username, $email, $hashed_password, $role, $otp);
